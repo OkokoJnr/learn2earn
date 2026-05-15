985474c36e37fbd45cf04ba0651c3345ddd5df9b
OkokoJnr <96591819+OkokoJnr@users.noreply.github.com>
2026-05-15 12:09:10 +0100

    function to identifies negative number

---
+++ piscine/quest2/isnegative.go
@@ -0,0 +1,21 @@
+package main
+// Write a function that prints 'T' (true) on a single line if the int passed as parameter is negative, otherwise it prints 'F' (false).
+
+
+import "github.com/01-edu/z01"
+
+func main(){
+	IsNegative(1)
+	IsNegative(0)
+	IsNegative(-1)
+
+}
+
+func IsNegative(nb int) {
+	if nb < 0 {
+		z01.PrintRune('T')
+	}else{
+		z01.PrintRune('F')
+	}
+	z01.PrintRune('\n')
+}
